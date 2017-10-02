e93e8df63e5518b4c0ffaa9943c5fdac7804e476
Ruben <RagnarZADev@gmail.com>
2017-10-02 18:00:25 +0200

    Test commit on master branch

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Smallshop
                 </div>
 
                 <div class="links">
